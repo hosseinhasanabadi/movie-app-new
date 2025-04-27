9791323b6a42d0f084afcbd467455d4c373cc477
Hossein <hhasnabadi@yahoo.com>
2025-04-27 15:33:02 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./components/Header";
+import { Outlet } from "react-router";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <main>
+      <Header />
+      <Outlet />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/ExplorePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ExplorePage() {
+  return (
+    <div>
+      ExplorePage
+    </div>
+  )
+}
+
+export default ExplorePage
